8b23923200652031955ae776223b03cf357611b3
raquelinsfran <raquelinsfran17@gmail.com>
2024-11-08 14:55:38 -0300

    Cambios nuevos

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <template>
   <div id="app">
     <!-- Barra de navegación con enlaces a diferentes rutas -->
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/addtask">Agregar Tarea</router-link> 
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link to="/">Home</router-link> |
+            </li>
+            <li class="nav-item">
+              <router-link to="/addtask">Agregar Tarea</router-link> |
+            </li>
+            <li class="nav-item">
+              <router-link to="/tasklist">Lista de Tareas</router-link> |
+            </li>
+            <li class="nav-item">
+              <router-link to="/combinedview">Vista Combinada</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
+
+
+
+
+
     <!-- Aquí se cargará el componente correspondiente a la ruta seleccionada -->
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
